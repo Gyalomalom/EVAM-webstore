cb1f3bf460dfb4301e2e3ff3103e4ceff9b2e7ee
Aleksandar_Nasev <aleknasev@yahoo.com>
2020-11-05 21:58:19 +0100

    Fixed formatting issues with images in mission page, adjusted size for index page

--- EvamBags/index.php
+++ EvamBags/index.php
@@ -17,9 +17,7 @@
             <div class = text><p>EVAM is a Hungarian brand. We started in 2013 and after a long break, we are back in business again. In this time we decided to do our best to reform our brand as sustainable as possible. It will be a long process to become a fully enviromental friendly brand, so we need your help.Part of our mission is help you become a conscious consumer. We start this process by producing our bags in Hungary instead of outsourcing to developing countries. Read more about our mission and values here.</p></div>   
         </div>
         <div class = imageparagraph id = p2>
-            <div class = text><p>Fashion Revolution is a nonprofit global movement represented by The Fashion Revolution Foundation and Fashion Revolution CIC with teams in over 100 countries around the world. Fashion Revolution campaigns for reform of the fashion industry with a focus on the need for greater transparency in the fashion supply chain.If you buy products from our webstore, after every purcase we donate of the total price to Fashion Revolution Foundation to reform the fashion industry in developing countries.
-
-</p></div>
+            <div class = text><p>Fashion Revolution is a nonprofit global movement represented by The Fashion Revolution Foundation and Fashion Revolution CIC with teams in over 100 countries around the world. Fashion Revolution campaigns for reform of the fashion industry with a focus on the need for greater transparency in the fashion supply chain.If you buy products from our webstore, after every purcase we donate of the total price to Fashion Revolution Foundation to reform the fashion industry in developing countries.</p></div>
             <div class = inlinePic><img src = "Images/placeholder%207x7.png" id = pi2></div>
         </div>
     </div>
--- EvamBags/mission.php
+++ EvamBags/mission.php
@@ -17,17 +17,17 @@
             <div class = text><p>Slow fashion describes an approach that takes myriad impacts into account – from resources, supply chains and living wages to environmental impact, pollution and animal life.To shop slowly and consciously is to buy less, choose well and invest in quality items that will stand the test of time from brands that advocate for fair treatment of people, animals and the planet.Slow fashion makes reference to the intention of our shopping experiences, how we care for our clothes, how long we help them to last and how we dispose of or recycle them.</p></div>
             <div class = inlinePic><img src = "Images/slow.jpg" id = pi2></div>
         </div>
-		<div class = imageparagraph id = p3>
-            <div class = text><p>Clothing items with rounded edges and patterns are responsible for a large part of waste produced by the fashion industry. For this reason, our design philosophy is centered around square shaped bags and accessories which means we produces effectively no waste compared to legacy fashion brands. </p></div>
+		<div class = imageparagraph id = p1>
             <div class = inlinePic><img src = "Images/low.jpg" id = pi6></div>
+            <div class = text><p>Clothing items with rounded edges and patterns are responsible for a large part of waste produced by the fashion industry. For this reason, our design philosophy is centered around square shaped bags and accessories which means we produces effectively no waste compared to legacy fashion brands. </p></div>
         </div>
-		<div class = imageparagraph id = p4>
+		<div class = imageparagraph id = p2>
             <div class = text><p>"Think globally, act locally" is the philosophy we wish to share with our customers. We firmly oppose the near slave like labor used by large corporations in the Middle East and Asia and manufacture all of our product in the European Union for fair wages and in safe environments.</p></div>
             <div class = inlinePic><img src = "Images/local.jpg" id = pi4></div>
         </div>
-		<div class = imageparagraph id = p5>
-            <div class = text><p>Vegan fashion is clothing and accessories made from cruelty-free sources, i.e. NO animal products were used in production of garments and gear, and no animals were harmed. Instead of buying leather clothing and leather accessories such belts, shoes, and purses - Look for products made with natural or man-made fiber like cotton, hemp, linen, microfiber, ramie, pleather, and imitation leather.</p></div>
+		<div class = imageparagraph id = p1>
             <div class = inlinePic><img src = "Images/vegan.jpg" id = pi5></div>
+            <div class = text><p>Vegan fashion is clothing and accessories made from cruelty-free sources, i.e. NO animal products were used in production of garments and gear, and no animals were harmed. Instead of buying leather clothing and leather accessories such belts, shoes, and purses - Look for products made with natural or man-made fiber like cotton, hemp, linen, microfiber, ramie, pleather, and imitation leather.</p></div>
         </div>
     </div>
 <?php include 'ContactInfo.php'?>
