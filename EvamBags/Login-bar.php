d0b3cb45feb9b59c5c99ece1016ffb288a82e442
David Horvath <david.horvath@student.fontys.nl>
2020-12-12 15:20:14 +0100

    Implemented grid layout template, area backgrounds are colored for better visibility

--- EvamBags/Login-bar.php
+++ EvamBags/Login-bar.php
@@ -1,4 +1,4 @@
-<div class = LoginBar>
-        <div class = LBitem><a href = "adminpanel.php" id = loglink>Admin panel</a></div>
-        <div class = LBitem><a href = "register.php" id = loglink>Create account</a></div>
-    </div>
+<div class = "LoginBar" id = "log">
+        <div class = "LBitem"><a href = "adminpanel.php" id = "loglink">Admin panel</a></div>
+        <div class = "LBitem"><a href = "register.php" id = "loglink">Create account</a></div>
+</div>
--- EvamBags/index.php
+++ EvamBags/index.php
@@ -5,23 +5,17 @@
     <title>Designer handbags</title>
 <link rel="stylesheet" type="text/css" href="Styles/Style.css">
 </head>
-<body>
-    <?php include 'Login-bar.php'?>
-    <?php include 'NavBar.html'; ?>
-    <div class = banner>
-    <img src = "Images/placeholder%208x2.5.png" id = ban1>
-    </div>
-    <div class = index-wrapper>
-        <div class = imageparagraph id = p1>
-            <div class = inlinePic><img src = "Images/ph%207x7.png" id = pi1></div>
-            <div class = text><p>EVAM is a Hungarian brand. We started in 2013 and after a long break, we are back in business again. In this time we decided to do our best to reform our brand as sustainable as possible. It will be a long process to become a fully enviromental friendly brand, so we need your help.Part of our mission is help you become a conscious consumer. We start this process by producing our bags in Hungary instead of outsourcing to developing countries. Read more about our mission and values here.</p></div>   
-        </div>
-        <div class = imageparagraph id = p2>
-            <div class = text><p>Fashion Revolution is a nonprofit global movement represented by The Fashion Revolution Foundation and Fashion Revolution CIC with teams in over 100 countries around the world. Fashion Revolution campaigns for reform of the fashion industry with a focus on the need for greater transparency in the fashion supply chain.If you buy products from our webstore, after every purcase we donate of the total price to Fashion Revolution Foundation to reform the fashion industry in developing countries.</p></div>
-            <div class = inlinePic><img src = "Images/placeholder%207x7.png" id = pi2></div>
-        </div>
-    </div>
-<?php include 'ContactInfo.php'?>
-</body>
-<footer>HTML and CSS for website written by Aleksandar Nasev and David Horvath.</footer>
-</html>
+<div class="container">
+  <nav><?php include 'Login-bar.php'?><?php include 'NavBar.html'; ?> </nav>
+  <main><img src = "Images/placeholder5.png"/></main>
+  
+  <div id="content1"><img src = "Images/ph2.png"/></div>
+  <div id="content2"><p>EVAM is a Hungarian brand. We started in 2013 and after a long break, we are back in business again. 
+						In this time we decided to do our best to reform our brand as sustainable as possible. It will be a long 
+						process to become a fully enviromental friendly brand, so we need your help.Part of our mission is help you 
+						become a conscious consumer. We start this process by producing our bags in Hungary instead of outsourcing to 
+						developing countries. Read more about our mission and values here.</p></div>   
+  <div id="content3">Content3</div>
+  <div id="content4"><img src = "Images/placeholder1.png"/></div>
+  <footer>Footer</footer>
+</div>
