8579ba02c1fc1b712d4b8538a6cde8a9934decd0
David Horvath <david.horvath@student.fontys.nl>
2020-12-12 22:37:40 +0100

    impemented revamped design

--- EvamBags/Login-bar.php
+++ EvamBags/Login-bar.php
@@ -1,4 +1,8 @@
-<div class = "LoginBar" id = "log">
-        <div class = "LBitem"><a href = "adminpanel.php" id = "loglink">Admin panel</a></div>
-        <div class = "LBitem"><a href = "register.php" id = "loglink">Create account</a></div>
-</div>
+
+<ul class="annbar">
+  <li><a href = "https://www.facebook.com/evamihalikdesign"><img src = "Images/icons8-facebook-24.png" id = "facebook"></a></li>
+  <li><a href = "https://www.instagram.com/evamihalikdesign"><img src = "Images/icons8-instagram-26.png" id = "ig"></a></li>
+  
+  <li style="float:right"><a href = "adminpanel.php"><img src = "Images/loginbtn.png" id = "login"></a></li>
+  <p>FREE SHIPPING ON ORDERS ABOVE €50!</p>
+</ul>
--- EvamBags/index.php
+++ EvamBags/index.php
@@ -6,16 +6,25 @@
 <link rel="stylesheet" type="text/css" href="Styles/Style.css">
 </head>
 <div class="container">
-  <nav><?php include 'Login-bar.php'?><?php include 'NavBar.html'; ?> </nav>
-  <main><img src = "Images/placeholder5.png"/></main>
+  <div id="announce"><?php include 'Login-bar.php'; ?></div>
+  <nav><?php include 'NavBar.html'; ?> </nav>
+  <img src = "Images/banner.jpg" id="banner"/>
+
+  <div id="annbar">  </div>
+
+  <img src = "Images/ph2.png" id="con1"/>
+  <div id="content2"><p>Sleek and unique
+                     Timeless and compeletely unique design combined with amazing practicality for your everday adventures.</p></div>   
+  <div id="content3"><p>Vegan and ethical Each bag hand made in Europe using 100% vegan and high quality materials.</p></div>
+						
+  <img src = "Images/placeholder1.png" id="con4"/>
+
+  <img src = "Images/con5.jpg" id="con5"/>
+
+  <div id="content5"><p>Ultra long life
+                        Our extremely high manufacturing standards ensure that your bag will always there for you.</p>
+  </div>
+
   
-  <div id="content1"><img src = "Images/ph2.png"/></div>
-  <div id="content2"><p>EVAM is a Hungarian brand. We started in 2013 and after a long break, we are back in business again. 
-						In this time we decided to do our best to reform our brand as sustainable as possible. It will be a long 
-						process to become a fully enviromental friendly brand, so we need your help.Part of our mission is help you 
-						become a conscious consumer. We start this process by producing our bags in Hungary instead of outsourcing to 
-						developing countries. Read more about our mission and values here.</p></div>   
-  <div id="content3">Content3</div>
-  <div id="content4"><img src = "Images/placeholder1.png"/></div>
   <footer>Footer</footer>
 </div>
